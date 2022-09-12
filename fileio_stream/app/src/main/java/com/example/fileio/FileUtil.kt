bce7a95c68ca8bfdc07e456356144d37e5cf59e1
Ryujin1210 <ywg6525@daum.net>
2022-09-12 18:52:50 +0900

    fileio_stream

---
+++ fileio_stream/app/src/main/java/com/example/fileio/FileUtil.kt
@@ -0,0 +1,38 @@
+package com.example.fileio
+
+import java.io.*
+import java.security.cert.CertPath
+
+class FileUtil {
+}
+
+fun readTextFile (fullPath: String) : String {
+    val file = File(fullPath)
+    if(!file.exists()) { return "" }
+
+    val reader = FileReader(file)
+    val buffer = BufferedReader(reader)
+
+    var temp = ""
+    val result = StringBuffer()
+
+    while(true) {
+        temp = buffer.readLine()
+        if (temp == null)
+            break
+        else result.append(buffer)
+    }
+    buffer.close()
+    return result.toString()
+}
+
+fun writeTextFile(directory: String, filename: String, content: String) {
+    val dir = File(directory)
+    if(!dir.exists()){
+        dir.mkdirs()
+    }
+    val writer = FileWriter(directory + "/" + filename)
+    val buffer = BufferedWriter(writer)
+    buffer.write(content)
+    buffer.close()
+}
